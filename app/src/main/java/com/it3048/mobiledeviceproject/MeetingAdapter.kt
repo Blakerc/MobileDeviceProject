2361113788545a856035503dffe92954878ca77d
momoney13 <64868507+momoney13@users.noreply.github.com>
2021-07-25 15:56:00 -0400

    Changed the object name to match Kotlin conventions

--- app/src/main/java/com/it3048/mobiledeviceproject/MeetingAdapter.kt
+++ app/src/main/java/com/it3048/mobiledeviceproject/MeetingAdapter.kt
@@ -8,20 +8,20 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class MeetingAdapter(val c:Context, val meetingList:ArrayList<MeetingDAO>):RecyclerView.Adapter<MeetingAdapter.meetingViewHolder>() {
-    inner class meetingViewHolder(val v:View):RecyclerView.ViewHolder(v){
+    inner class MeetingViewHolder(val v:View):RecyclerView.ViewHolder(v){
         val title = v.findViewById<TextView>(R.id.meetingNameTxt)
         val date = v.findViewById<TextView>(R.id.meetingDateTxt)
         val description = v.findViewById<TextView>(R.id.meetingDescriptionTxt)
         val link = v.findViewById<TextView>(R.id.meetingUrlTxt)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): meetingViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MeetingViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val v = inflater.inflate(R.layout.meeting_card, parent, false)
-        return meetingViewHolder(v)
+        return MeetingViewHolder(v)
     }
 
-    override fun onBindViewHolder(holder: meetingViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: MeetingViewHolder, position: Int) {
         val newList = meetingList[position]
         holder.title.text = newList.meetingTitle
         holder.date.text = newList.meetingDate
