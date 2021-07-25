68c55672d517f1daddb350b6a1d30e34e4bf1b13
momoney13 <64868507+momoney13@users.noreply.github.com>
2021-07-25 14:32:08 -0400

    Changed Array line
    arrayOf is outdated, so I switched it to a literal

--- app/src/main/java/com/it3048/mobiledeviceproject/dao/MeetingDatabase.kt
+++ app/src/main/java/com/it3048/mobiledeviceproject/dao/MeetingDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.it3048.mobiledeviceproject.dto.Meeting
 
-@Database(entities = arrayOf(Meeting::class), version = 1)
+@Database(entities = [Meeting::class], version = 1)
 abstract class MeetingDatabase : RoomDatabase() {
     abstract fun localMeetingDAO() : MeetingDAO
 }
